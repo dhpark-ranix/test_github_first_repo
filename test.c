2d8ab402bb3129ae1737fd11e015683c29c35127
dhpark-ranix <dhpark@ranix.co.kr>
2026-03-06 09:52:43 +0900

    Conflict test

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 int main(){
     printf("hello\n");
     printf("branch1\n");
-    printf("branch2\n");
-    printf("branch2_2\n");
+    //printf("branch2\n");
+    //printf("branch2_2\n");
     return 0;
 }
